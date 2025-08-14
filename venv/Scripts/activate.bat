c45d30cfa41e7d675d15e0527db6973be5fea2e8
ezrayalley <153390544+ezrayalley@users.noreply.github.com>
2025-08-14 18:37:41 +0530

    n

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\MY FILES\My Coding Projects\Sciviz Project\venv
+set VIRTUAL_ENV=D:\MY FILES\My Coding Projects\Sciviz Project\Sci-Viz--AI-Powered-Science-Visualization-Tool\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
